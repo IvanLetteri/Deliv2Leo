05ee93d06dc57d716cdaf60b201893743e915d40
Ivan Letteri <ivan.informatica09@gmail.com>
2020-01-09 14:28:18 +0100

    Update DNNshape.py

--- Utils/DNNshape.py
+++ Utils/DNNshape.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 from keras.models import Sequential
 
 class DNNshape:
-  def create_model(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
+  def rect_dnn(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
     model=tf.keras.models.Sequential()
     model.add(tf.keras.layers.Dense((inLayers*2), input_dim=(inLayers-outLayers), activation=actFunc))
     model.add(tf.keras.layers.Dropout(dropOut))
 
     for i in range(0,numLayers):
-      model.add(tf.keras.layers.Dense(inLayers, kernel_initializer=kerInit, activation=actFunc))
+      model.add(tf.keras.layers.Dense((inLayers*2), kernel_initializer=kerInit, activation=actFunc))
       model.add(tf.keras.layers.Dropout(dropOut))
 
     model.add(tf.keras.layers.Dense(outLayers, kernel_initializer=kerInit, activation=outActFunc))
