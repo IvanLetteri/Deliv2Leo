76e8f95d7b372115f9bed743d5e53dabc6e57f66
Ivan Letteri <ivan.informatica09@gmail.com>
2020-01-11 20:12:51 +0100

    Update DNNshape.py

--- Utils/DNNshape.py
+++ Utils/DNNshape.py
@@ -1,32 +1,45 @@
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dropout, BatchNormalization
+from keras.layers.normalization import BatchNormalization
+
+'''
+model.add(Dense(64, input_dim=14, init='uniform'))
+model.add(BatchNormalization())
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+'''
 
 class DNNshape:
   def rect_dnn(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
     model=tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Dense((inLayers*2), input_dim=(inLayers-outLayers), activation=actFunc))
+    model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), input_dim=(inLayers-outLayers)))
     model.add(BatchNormalization())
+    model.add(Activation(actFunc))
     model.add(tf.keras.layers.Dropout(dropOut))
 
     for i in range(0,numLayers):
-      model.add(tf.keras.layers.Dense((inLayers*2), kernel_initializer=kerInit, activation=actFunc))
+      model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), kernel_initializer=kerInit))
       model.add(BatchNormalization())
+      model.add(Activation(actFunc))
       model.add(tf.keras.layers.Dropout(dropOut))
 
-    model.add(tf.keras.layers.Dense(outLayers, kernel_initializer=kerInit, activation=outActFunc))
+    model.add(tf.keras.layers.Dense(outLayers, kernel_initializer=kerInit))
+    model.add(Activation(actFunc))
     return model
   
   def diam_dnn(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
     model=tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Dense((inLayers*2), input_dim=(inLayers-outLayers), activation=actFunc))
+    model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), input_dim=(inLayers-outLayers)))
     model.add(BatchNormalization())
+    model.add(Activation(actFunc))
     model.add(tf.keras.layers.Dropout(dropOut))
 
     for i in range(0,numLayers):
-      model.add(tf.keras.layers.Dense((inLayers/(i+1)), kernel_initializer=kerInit, activation=actFunc))
+      model.add(tf.keras.layers.Dense(((inLayers-outLayers)/(i+1)), kernel_initializer=kerInit))
       model.add(BatchNormalization())
+      model.add(Activation(actFunc))
       model.add(tf.keras.layers.Dropout(dropOut))
 
-    model.add(tf.keras.layers.Dense(outLayers, kernel_initializer=kerInit, activation=outActFunc))
+    model.add(tf.keras.layers.Dense(outLayers, kernel_initializer=kerInit))
+    model.add(Activation(actFunc))
     return model
