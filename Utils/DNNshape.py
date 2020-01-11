f4c4b11ce85186749cf3b9e77d4c0b7627faa409
Ivan Letteri <ivan.informatica09@gmail.com>
2020-01-11 20:23:29 +0100

    Update DNNshape.py

--- Utils/DNNshape.py
+++ Utils/DNNshape.py
@@ -7,13 +7,13 @@ class DNNshape:
   def rect_dnn(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
     model=tf.keras.models.Sequential()
     model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), input_dim=(inLayers-outLayers)))
-    model.add(tf.keras.layers.BatchNormalization())
+    #model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Activation(actFunc))
     model.add(tf.keras.layers.Dropout(dropOut))
 
     for i in range(0,numLayers):
       model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), kernel_initializer=kerInit))
-      model.add(tf.keras.layers.BatchNormalization())
+      #model.add(tf.keras.layers.BatchNormalization())
       model.add(tf.keras.layers.Activation(actFunc))
       model.add(tf.keras.layers.Dropout(dropOut))
 
@@ -24,13 +24,13 @@ class DNNshape:
   def diam_dnn(self, inLayers=22, outLayers=2, numLayers=2, actFunc='relu', kerInit='normal', dropOut=0.2, outActFunc='softmax', multFactor=1):
     model=tf.keras.models.Sequential()
     model.add(tf.keras.layers.Dense(((inLayers-outLayers)*2), input_dim=(inLayers-outLayers)))
-    model.add(tf.keras.layers.BatchNormalization())
+    #model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Activation(actFunc))
     model.add(tf.keras.layers.Dropout(dropOut))
 
     for i in range(0,numLayers):
       model.add(tf.keras.layers.Dense(((inLayers-outLayers)/(i+1)), kernel_initializer=kerInit))
-      model.add(tf.keras.layers.BatchNormalization())
+      #model.add(tf.keras.layers.BatchNormalization())
       model.add(tf.keras.layers.Activation(actFunc))
       model.add(tf.keras.layers.Dropout(dropOut))
 
