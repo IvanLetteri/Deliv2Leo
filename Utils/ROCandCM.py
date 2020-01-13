7de0ed6f0339fb7c001cfba12f0cf75b68703ed1
Ivan Letteri <ivan.informatica09@gmail.com>
2020-01-13 19:31:31 +0100

    Update ROCandCM.py

--- Utils/ROCandCM.py
+++ Utils/ROCandCM.py
@@ -1,5 +1,5 @@
 class ROCandCM:
-  def mcor(self, test_class, predict_class):
+  def rocCM(self, test_class, predict_class):
     false_positive_rate, recall, thresholds = roc_curve(test_class, predict_class)
     roc_auc = auc(false_positive_rate, recall)
     cm = confusion_matrix(test_class, predict_class)
