772cfb496d97cdcb7acb1c1046fc5d99584c597a
Ivan Letteri <ivan.informatica09@gmail.com>
2020-01-13 19:55:45 +0100

    Update ROCandCM.py

--- Utils/ROCandCM.py
+++ Utils/ROCandCM.py
@@ -5,7 +5,7 @@ from sklearn.metrics import precision_recall_fscore_support as score
 class ROCandCM:
   def rocCM(self, test_class, predict_class):
     fpr, recall, thresholds = roc_curve(test_class, predict_class)
-    roc_auc = auc(false_positive_rate, recall)
+    roc_auc = auc(fpr, recall)
     cm = confusion_matrix(test_class, predict_class)
     
     return fpr, recall, roc_auc, cm
